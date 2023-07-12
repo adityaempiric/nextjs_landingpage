2bee2c2505739e2109f595af256055973e7e933a
adityaempiric <ad.empiricinfotech@gmail.com>
2023-07-12 10:01:29 +0530

    applied themes

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+"use client"
+
+import React, { useContext } from 'react'
 import styles from './page.module.css';
 import Image from 'next/image';
 import contactImg from '/public/contact.png'
 import Button from '@/components/Button/Button';
+import { ThemeContext } from '@/context/ThemeContext';
 
 const Contact = () => {
+
+  const {toggle,mode} = useContext(ThemeContext)
+
   return (
     <div className={styles.container}>
         <div className={styles.title}>
@@ -13,9 +19,9 @@ const Contact = () => {
         <div className={styles.content}>
           <Image src={contactImg} width={500} height={500} className={styles.img}  />
           <form className={styles.form}>
-            <input type="text" placeholder='Name' className={styles.input} />
-            <input type="text" placeholder='Email' className={styles.input} />
-            <textarea rows="10" cols="30" placeholder='message' className={styles.textarea}></textarea>
+            <input type="text" placeholder='Name' className={mode === "dark" ? styles.input :styles.darkInput} />
+            <input type="text" placeholder='Email' className={mode === "dark" ? styles.input :styles.darkInput} />
+            <textarea rows="10" cols="30" placeholder='message' className={mode === "dark" ? styles.textarea :styles.darkTextarea}></textarea>
             <Button url="#" text="Send" />
           </form>
         </div>
